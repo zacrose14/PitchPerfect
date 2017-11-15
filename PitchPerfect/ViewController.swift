b678c472412a5f778d7ce685dd101bd63e546ffa
Zac Rose <zac@insightmobiledevelopment.com>
2017-11-15 11:14:42 -0500

    All button assets loaded. Buttons linked to various controls needed to start/stop recording.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,13 +31,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        
-        print("Record Button Pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     @IBAction func stopRecording(_ sender: Any) {
-        
-        print("recording stopped")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
     
 }
